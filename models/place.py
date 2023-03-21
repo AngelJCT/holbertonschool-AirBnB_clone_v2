99a07b3d27462a1a7c1084e324c005b1794d1fbb
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-21 14:23:42 -0400

    add reviews to places

--- models/place.py
+++ models/place.py
@@ -4,6 +4,8 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
+from models.review import Review
+from models import storage
 
 
 class Place(BaseModel, Base):
@@ -20,6 +22,7 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
+        reviews = relationship("Review", backref="place", cascade="all, delete")
     else:
         city_id = ""
         user_id = ""
@@ -32,5 +35,11 @@ class Place(BaseModel, Base):
         latitude = 0.0
         longitude = 0.0
 
+        @property
+        def reviews(self):
+            return [obj for obj in
+                    storage.all(Review).values()
+                    if obj.place_id == self.id]
+
     amenity_ids = []
-    reviews = relationship("Review", backref="place", cascade="all, delete")
+    
--- models/review.py
+++ models/review.py
@@ -3,11 +3,18 @@
 from models.base_model import BaseModel, Base
 from models.place import Place
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Review(BaseModel):
     """ Review classto store review information """
     __tablename__ = 'reviews'
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    else:
+        text = ""
+        place_id = ""
+        user_id = ""
