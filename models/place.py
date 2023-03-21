cf6f5029eb0f415cb92f3c2e085764a2896d56ee
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-21 12:59:17 -0400

    Include Place in SQL itegrations

--- models/place.py
+++ models/place.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, \
+                       Integer, Float
 
 
 class Place(BaseModel, Base):
@@ -9,12 +10,12 @@ class Place(BaseModel, Base):
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    name = 
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=False, default=0)
+    longitude =Column(Float, nullable=False)
     amenity_ids = []
