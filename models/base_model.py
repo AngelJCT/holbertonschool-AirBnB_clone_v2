a356a2dcd942543853ff59d4964658edbd45f1ea
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-22 11:24:41 -0400

    Verify the kwargs put into BaseModel (or its children) are valid based on the classes' __dict__

--- models/base_model.py
+++ models/base_model.py
@@ -18,25 +18,29 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        if kwargs:
-            #In def __init__(self, *args, **kwargs):, manage kwargs to create instance attribute from this dictionary. Ex: kwargs={ 'name': "California" } => self.name = "California" if it’s not already the case
-            if 'id' not in kwargs:
-                self.id = str(uuid.uuid4())
-            if 'created_at' not in kwargs:
-                self.created_at = datetime.now()
-            if 'updated_at' not in kwargs:
-                self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
-            for k, v in kwargs.items():
-                if k in ['created_at', 'updated_at']:
-                    t_format = '%Y-%m-%dT%H:%M:%S.%f'
-                    self.__dict__[k] = datetime.strptime(v, t_format)
-                elif k != '__class__':
-                    self.__dict__[k] = v
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
+        for k, v in kwargs.items():
+            if k == 'id':
+                self.id = v
+            elif k in ['created_at', 'updated_at']:
+                t_format = '%Y-%m-%dT%H:%M:%S.%f'
+                self.__dict__[k] = datetime.strptime(v, t_format)
+            elif k == '__class__':
+                pass
+            elif k in self.__dict__ and isinstance(v, type(self.__dict__[k])):
+                # We don't want any "custom" attribute created
+                # by the user of the console, other than the attributes
+                # specified in the classes' files.
+                self.__setattr__(k, v)
+            else:
+                raise KeyError(f"Invalid attribute key:value pair for '{type(self)}': {k}:{v}")
+        if kwargs:
             self.updated_at = datetime.now()
+            # If the kwargs were used, we're ALWAYS supposed
+            # to make 'updated_at' be now.
 
     def __str__(self):
         """Returns a string representation of the instance"""
