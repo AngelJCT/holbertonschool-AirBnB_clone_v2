b195fe87b8320ade03b15d10e1910030ff434f72
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-22 09:40:06 -0400

    Add Base as parent class of Review, to allow it to function as a table

--- models/review.py
+++ models/review.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
-from models.place import Place
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
 from os import getenv
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
     if getenv("HBNB_TYPE_STORAGE") == "db":
