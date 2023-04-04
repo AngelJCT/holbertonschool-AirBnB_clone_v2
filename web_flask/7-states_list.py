bdf68184c4be86ece3a9f7d50c8d2dc39be729ee
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-04 12:12:02 -0400

    fixing states_list method

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,14 +2,13 @@
 """Starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Displays a HTML page with a list of states"""
-    states = storage.all(State).values()
+    states = storage.all("State").values()
     return render_template('7-states_list.html', states=states)
 
 
