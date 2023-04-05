36db2c4f5fad6e3d048574dff360787024502429
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-04-04 23:57:50 -0400

    fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Starts a Flask web application"""
 from flask import Flask, render_template
+from models.state import State
 from models import storage
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """Displays a HTML page with a list of states"""
-    states = storage.all("State").values()
+    states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
 
 
